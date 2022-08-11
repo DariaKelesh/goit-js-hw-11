cc6a1496161392a3e508bb11c829205218a24cf6
DariaKelesh <75041274+DariaKelesh@users.noreply.github.com>
2022-08-11 19:00:35 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import axios from "axios";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 
+const KEY = "29175457-ea8e2c93dbfac842acac0bec2";
+const URL = `https://pixabay.com/api/?${KEY}&q=${q}&image_type=photo&orientation=horizontal&safesearch=true`;
+
+// разобраться с q!!!
+
+// https://pixabay.com/api/?key=29175457-ea8e2c93dbfac842acac0bec2&q=yellow+flowers&image_type=photo
+
+
 // key - твой уникальный ключ доступа к API.
 // q - термин для поиска. То, что будет вводить пользователь.
 // image_type - тип изображения. Мы хотим только фотографии, поэтому задай значение photo.
