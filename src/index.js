0512b0bfa7eefb17c031a6140d46572ef0257784
DariaKelesh <75041274+DariaKelesh@users.noreply.github.com>
2022-08-14 11:08:41 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,88 @@
-import axios from "axios";
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// import axios from "axios";
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 
-const KEY = "29175457-ea8e2c93dbfac842acac0bec2";
-const URL = `https://pixabay.com/api/?${KEY}&q=${q}&image_type=photo&orientation=horizontal&safesearch=true`;
+// const KEY = "29175457-ea8e2c93dbfac842acac0bec2";
+// const URL = `https://pixabay.com/api/?${KEY}&q=${q}&image_type=photo&orientation=horizontal&safesearch=true`;
 
-// разобраться с q!!!
+import { fetchImages } from './js/fetchApi.js';
+import { renderGallery } from './js/render.js';
+import { onScroll, onToTopBtn } from './js/scroll.js';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-// https://pixabay.com/api/?key=29175457-ea8e2c93dbfac842acac0bec2&q=yellow+flowers&image_type=photo
+const searchForm = document.querySelector('#search-form');
+const gallery = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.button--loadMore');
+let query = '';
+let page = 1;
+let simpleLightBox;
+const perPage = 40;
 
+searchForm.addEventListener('submit', onSearchForm);
+loadMoreBtn.addEventListener('click', onLoadMoreBtn);
 
-// key - твой уникальный ключ доступа к API.
-// q - термин для поиска. То, что будет вводить пользователь.
-// image_type - тип изображения. Мы хотим только фотографии, поэтому задай значение photo.
-// orientation - ориентация фотографии. Задай значение horizontal.
-// safesearch - фильтр по возрасту. Задай значение true.
+onScroll();
+onToTopBtn();
+
+function onSearchForm(e) {
+  e.preventDefault();
+  window.scrollTo({ top: 0 });
+  page = 1;
+  query = e.currentTarget.searchQuery.value.trim();
+  gallery.innerHTML = '';
+  loadMoreBtn.classList.add('is-hidden');
+
+  if (query === '') {
+    Notiflix.Notify.failure(
+      'The search string cannot be empty. Please specify your search query.'
+    );
+    return;
+  }
+
+  fetchImages(query, page, perPage)
+    .then(({ data }) => {
+      if (data.totalHits === 0) {
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      } else {
+        renderGallery(data.hits);
+        simpleLightBox = new SimpleLightbox('.gallery a').refresh();
+
+        Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+
+        if (data.totalHits > perPage) {
+          loadMoreBtn.classList.remove('is-hidden');
+        }
+      }
+    })
+    .catch(error => console.log(error));
+  // e.currentTarget.reset;
+  searchForm.reset();
+}
+
+function onLoadMoreBtn() {
+  page += 1;
+  simpleLightBox.destroy();
+
+  fetchImages(query, page, perPage)
+    .then(({ data }) => {
+      renderGallery(data.hits);
+      simpleLightBox = new SimpleLightbox('.gallery a').refresh();
+
+      const totalPages = Math.ceil(data.totalHits / perPage);
+
+      if (page >= totalPages) {
+        loadMoreBtn.classList.add('is-hidden');
+
+        Notiflix.Notify.failure(
+          "We're sorry, but you've reached the end of search results."
+        );
+      }
+    })
+    .catch(error => console.log(error));
+}
+
+searchForm.reset();
